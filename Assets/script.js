2f906819494b5a62febfd2ad388db5574605bf8e
Juliana Andrews <julianakandrews@gmail.com>
2020-08-30 12:44:16 -0700

    Added extra comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,21 @@
+//Selects the full document and gets it ready for any click events
 $(document).ready(function(){
 
 let today = moment().format("MMM Do YY"); 
 let city;
+//Query selector of all html tags with the class = "main-dash"
 var main = $(".main-dash");
 
 //Retrieve any saved locations into loal storage
 let lastSearched = localStorage.getItem("city");
 
+//IF lastSearched = true
 if(lastSearched) {
+
+//Query selector for all HTML button tags
 let savedBtn = $('<button>');
 
+    //Set the "id" attribute on the selected <button> tags to the value of lastSearched (aka a string that is the city)
     savedBtn.attr("id", lastSearched);
     savedBtn.addClass("city-button");
     savedBtn.text(lastSearched);
@@ -18,10 +24,13 @@ let savedBtn = $('<button>');
 }
 
 function findWeather() {
+    //Prevents the auto-refresh from happening when a button is clicked and this function is run
     event.preventDefault();
 
+    //Unhide the area for weather
     main.attr("style", "opacity: 1;")
 
+    //api for finding the weather
     var apiKey = "2b0a60281cd72b23b54eac2117dc2c30";
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
@@ -96,6 +105,8 @@ $("#search-button").on("click", function(event){
     }
 });
 
+//This says SELECT the full body tag, and if the user clicks a tag with the class = "city-button", take what the id is
+//equal to (which is a city name) and plug into the findWeather() function to find the weather 
 $("body").on("click", ".city-button", function(event){
     city = $(this).attr("id");
     
